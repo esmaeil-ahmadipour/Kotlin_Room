47890f98a2c987c5b3c4206301af0d41d5d088a4
software8686 <software8686@gmail.com>
2019-12-25 17:52:43 +0330

    Commit> Insert Data And View Data with Use Ui Interface.

--- app/src/main/java/ir/sample/kotlinroom/MainActivity.kt
+++ app/src/main/java/ir/sample/kotlinroom/MainActivity.kt
@@ -1,62 +1,96 @@
 package ir.sample.kotlinroom
 
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
 import ir.sample.kotlinroom.roomLayer.AppDatabase
+import ir.sample.kotlinroom.roomLayer.dataAccessObjects.UserDao
 import ir.sample.kotlinroom.roomLayer.entities.UserEntity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    val context = this
+
+    var alertMessage = "";
+    var userId: Int = 0
+    val context: Context = this;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-        test();
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        listeners()
+    }
 
+
+    private fun listeners() {
+        // OnClick btn_main_confirm :
+        /*
+           1-Check Validate Data ,
+           2-Show AlertDialog For Errors ,
+           3-Send Data To Database & Intent Other Activity
+       */
+        btn_main_confirm.setOnClickListener {
+            if (validator()) {
+                val name: String = edt_main_name.text.toString()
+                val email: String = edt_main_email.text.toString()
+                val number: String = edt_main_number.text.toString()
+                sendData(name, email, number)
+                intentLoadData()
+            } else {
+                val invalidatedAlert = AlertDialog.Builder(context)
+                invalidatedAlert.setTitle(context.resources.getString(R.string.string_alert_title))
+
+                invalidatedAlert.setMessage(alertMessage)
+                invalidatedAlert.setNeutralButton(context.resources.getString(R.string.string_confirm) ,
+                    object : DialogInterface.OnClickListener {
+                        override fun onClick(dialog: DialogInterface?, p1: Int) {
+                            dialog?.dismiss();
+                        }
+                    })
+                invalidatedAlert.create().show()
+            }
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
+    //Check Validate Data Entries And Set Acceptance for them.
+    private fun validator(): Boolean {
+        var validate = true
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        if (edt_main_name.text.toString() == "") {
+            validate = false
+            alertMessage = context.resources.getString(R.string.string_alert_edtUsername)
         }
+
+        if (((edt_main_email.text.toString() != "") && !(edt_main_email.text.toString().contains("@"))))
+        {validate = false
+            alertMessage =context.resources.getString(R.string.string_alert_edtEmail)
+        }
+
+
+        if (edt_main_number.text.toString() == "")
+        { validate = false
+            alertMessage = context.resources.getString(R.string.string_alert_edtNumber)
+        }
+
+        return validate
     }
 
-    fun test() {
-        val db: AppDatabase = AppDatabase.getInstance(context)
-// Test : Insert New Record To Database
-/*
-        val userSampleInsert = UserEntity("Admin", "Admin@gmail.com", "09112223344")
-        db.user().insert(userSampleInsert)
-        val dbGetAllData =  db.user().getAll();
-*/
+    //Pass Received Data To New UserEntity Object And Use This Object To Insert Data.
+    private fun sendData(name: String, email: String, number: String) {
+        val userData: UserDao = AppDatabase.getInstance(context).user()
+        val user = UserEntity(userId, name, email, number)
+        userData.insert(user)
+    }
 
-
-// Test : Update One Record From Database
-/*
-        val userSampleInsert = UserEntity(1,"John","John@gmail.com","09012223344")
-        db.user().update(userSampleInsert)
-        val dbGetAllData =  db.user().getAll();
-*/
-
-        val dbGetData =  db.user().getById(1);
-
+    // Intent to LoadDataActivity
+    private fun intentLoadData() {
+        val loadData = Intent(context, LoadDataActivity::class.java)
+        startActivity(loadData)
     }
 }
+
+
+
+
